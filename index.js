3ef2a97eaf1f550801e205839139e8bef16a9eef
calkno <36138660+calkno@users.noreply.github.com>
2023-09-26 12:18:40 -0400

    working sub

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ client.on('messageCreate', message => {
   const user = message.author;
     if (message.content === 'KIR') {
       message.channel.send(`<@${user.id}> , Hell yeah I'm keeping it real`);
-      console.log('Processed ' + message.content)
+      //console.log('Processed ' + message.content)
 
     }
   
     if (message.content.includes('!s') && user.bot === false) {
       var response = message.content.replace("!s", "").split('/');
       var cleanStr = response[0];
-      console.log(cleanStr);
+      //console.log(cleanStr);
       const channel = client.channels.cache.get("1155291371977068634");
       channel.messages.fetch({ limit: 20 }).then(messages => {
       //const query = (msg) => msg.content.indexOf(cleanStr) > 0;
@@ -26,14 +26,17 @@ client.on('messageCreate', message => {
      
       const matching = messages.filter(query);
       let values = Array.from(matching.values());
-      console.log(values);
+      //console.log(values);
       for(let i=0;i < values.length;i++)
         if(values[i].toString().indexOf('!s') < 0){
           //console.log(values[i].content.replace(cleanStr, response[1]))
-          const author = values[i].author.username;
-          message.channel.send('@'+author +' '+ values[i].content.replace(cleanStr, ' '+response[1]));
+          const author = values[i].author.toString();
+          //console.log(author);
+          message.channel.send(author+ ' '+values[i].content.replace(cleanStr, ' '+response[1]));
+          break;
+        } else {
+          message.channel.send('uhh, stop that!');
         }
-        
       })
         
       }
