f159bf583656a473b1d2d146c89bbdf2dadf5f5c
Justin Dearing <zippy1981@gmail.com>
2023-10-05 22:28:34 -0400

    more and actually accurate config

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+const messesageFetchCount = process.env.MESSAGE_FETCH_COUNT;
+const oneBlockedPercent = process.env.ONE_BLOCKED_PERCENT;
+const realestOneBlockedPercentneBlockedPercent = process.env.RealestOneBlockedPercent;
+
 module.exports = {
     /**
      * Set to true to allow !configDump
@@ -7,20 +11,25 @@ module.exports = {
     AllowConfigDump: process.env.ALLOW_CONFIG_DUMP?.localeCompare('True', 'en', { sensitivity: 'base' }) === 0,
 
     /**
-     * semicolon separated list of people that get the Keroac5 treatment.
+     * Array of people that get the Keroac5 treatment.
      */
     TheRealests: process.env.THE_REALEST_MFERS?.split(';') ??  [ 'kerouac5' ],
+    
+    /**
+     * Number of messages to fetch when retrieving history.
+     */
+    MessageFetchCount: (messesageFetchCount > 0) ? messesageFetchCount : 50,
     
     /**
      * Percentage change of getting "who is one blocked message" for plebians who should be so lucky to be in
      * the present of so much KIR.
      */
-    OneBlockedPercent: Number(process.env.ONE_BLOCKED_PERCENT) ?? 1,
+    OneBlockedPercent: (oneBlockedPercent > 0) ? oneBlockedPercent : 1,
     
     /**
      * Percentage change of getting "who is one blocked message" for the realest.
      */
-    RealestOneBlockedPercent: Number(process.env.ONE_BLOCKED_PERCENT) ?? 5,
+    RealestOneBlockedPercent: (realestOneBlockedPercentneBlockedPercent > 0) ? realestOneBlockedPercentneBlockedPercent : 5,
 
     /**
      * The discord token.
